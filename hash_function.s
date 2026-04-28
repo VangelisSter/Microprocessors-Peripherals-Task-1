59c81537b552b2abb90627156d9d5abc7fc8f142
Evangelos Stergiopoulos <stergiopoulos@aristurtle.com>
2026-04-28 19:15:59 +0300

    Initial commit to this repo. Added .gitignore

---
+++ hash_function.s
@@ -0,0 +1,7 @@
+	.global hash_function
+	.p2align 2
+	.type hash_function,%function
+		
+		hash_function:
+			.fnstart
+			
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//extern int hash_function(char pin[]);
+
+int main(void){
+	char pin[] = "A382";
+	
+	printf("Pin is: %s", pin);
+	return 0;
+}
